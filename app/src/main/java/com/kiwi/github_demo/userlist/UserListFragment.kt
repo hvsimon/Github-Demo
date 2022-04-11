be8679617952dadc9532cd2f4565b8fc4513068f
HvSimon <a309011@gmail.com>
2022-04-11 23:28:40 +0800

    Add UserListFragment

---
+++ app/src/main/java/com/kiwi/github_demo/userlist/UserListFragment.kt
@@ -0,0 +1,15 @@
+package com.kiwi.github_demo.userlist
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.kiwi.github_demo.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UserListFragment : Fragment(R.layout.user_list_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // TODO: bind view
+    }
+}
